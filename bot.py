4cd2be180c47f1326ef8ad7c0b1ff2fcc89efce8
mojobojo77 <48215400+mojobojo77@users.noreply.github.com>
2019-03-04 19:23:00 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import asyncio
 from itertools import cycle
+import random
+import os
 
 bot = commands.Bot(command_prefix='.')
 bot.remove_command('help')
